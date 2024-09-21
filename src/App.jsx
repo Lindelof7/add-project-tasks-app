832aded746e6ad6b051a97e5d7b9c524dd625092
Volodymyr Butyter <butytervolodia@gmail.com>
2024-09-21 17:47:24 +0200

    working demo version on app
    Now you can add and delete projects. add and delete tasks

--- src/App.jsx
+++ src/App.jsx
@@ -4,105 +4,140 @@ import { ProjectsList } from "./components/ProjectsList";
 import { CreateProject } from "./components/CreateProject";
 import { Project } from "./components/Project";
 
-// let projects = { 
-//   project1:{
-//     title : 'Learn React',
-//     description: 'Efficiently, including frameworks and libraries',
-//     dueDate : 'Nov 1, 2024',
-//     tasks:[
-//       {taskName : 'Learn Tailwind',
-//         id: 1
-//       },
-//       {taskName : 'Learn TS',
-//         id: 2
-//       },
-//     ]
-//   },
-//   project2:{
-//     title : 'Learn Node',
-//     description: 'Efficiently, including frameworks and libraries',
-//     dueDate : 'Jan 1, 2024',
-//     tasks:[
-//       {taskName : 'Learn Express',
-//         id: 1
-//       },
-//       {taskName : 'Learn more',
-//         id: 2
-//       },
-//     ]
-//   }
-// }
-
-const projects = [
-  {
-    title : 'Learn React',
-    description: 'Efficiently, including frameworks and libraries',
-    dueDate : 'Nov 1, 2024',
-    id : 1,
-    tasks:[
-      {taskName : 'Learn Tailwind',
-        id: 1
-      },
-      {taskName : 'Learn TS',
-        id: 2
-      },
-    ]
-  },
-  {
-    title : 'Learn Node',
-    description: 'Efficiently, including frameworks and libraries',
-    dueDate : 'Jan 1, 2024',
-    id : 2,
-    tasks:[
-      {taskName : 'Learn Express',
-        id: 1
-      },
-      {taskName : 'Learn more',
-        id: 2
-      },
-    ]
-  }
-]
-
-
 function App() {
-  const [addingProject, setAddingProject] = useState(false);
-  const [viewingProject,setViewingProject] = useState(false)
-  const [selectedProject, setSelectedProject] = useState(null);
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [
+      {
+        title: "Learn React",
+        description: "Efficiently, including frameworks and libraries",
+        dueDate: "Nov 1, 2024",
+        id: Math.random(),
+        tasks: [
+          { taskName: "Learn Tailwind", id: Math.random() },
+          { taskName: "Learn TS", id: Math.random() },
+        ],
+      },
+      {
+        title: "Learn Node",
+        description: "Efficiently, including frameworks and libraries",
+        dueDate: "Jan 1, 2024",
+        id: Math.random(),
+        tasks: [
+          { taskName: "Learn Express", id: Math.random() },
+          { taskName: "Learn more", id: Math.random() },
+        ],
+      },
+    ],
+  });
 
-  function addProjectStatusTrue() {
-    setAddingProject(true)
+  function createTask(newTask, selectedProject) {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        projects: [...prevState.projects, selectedProject.tasks.push(newTask)],
+      };
+    });
   }
 
-  function finishAddingProject() {
-    setAddingProject(false)
-  } 
-
-  function viewProjectStatus() {
-    setViewingProject(true)
-    setAddingProject(false)
+  function deleteTask(taskId, selectedProject) {
+    setProjectState((prevState) => {
+      const updatedTasks = selectedProject.tasks.filter(
+        (task) => task.id !== taskId
+      );
+      return {
+        ...prevState,
+        projects: [
+          ...prevState.projects,
+          (selectedProject.tasks = updatedTasks),
+        ],
+      };
+    });
   }
 
-  function deleteProject(selectedProject) {
-    const selectedIndex = selectedProject.id - 1;
+  function handleStartAppProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
 
-    console.log(selectedIndex)
+  function createNewProject(newProject) {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
 
-    if (selectedIndex !== -1) {
-      projects.splice(selectedIndex, 1);
-    }
+  function deleteProject(projectId) {
+    setProjectState((prevState) => {
 
-console.log(projects);
+      const updatedProjects = prevState.projects.filter(
+        (project) => project.id !== projectId
+      );
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: updatedProjects,
+      };
+    });
+  }
+
+  function cancelProjectCreation() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+
+  function handleChooseProject(selectedProjectId) {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: selectedProjectId,
+      };
+    });
+  }
+
+  let content;
+  if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectsPage handleStartAppProject={handleStartAppProject} />;
+  } else if (projectState.selectedProjectId === null) {
+    content = (
+      <CreateProject
+        projects={projectState.projects}
+        createNewProject={createNewProject}
+        cancelProjectCreation={cancelProjectCreation}
+      />
+    );
+  } else if (projectState.selectedProjectId) {
+    content = (
+      <Project
+      deleteProject={deleteProject}
+        createTask={createTask}
+        deleteTask={deleteTask}
+        selectedProject={projectState.selectedProjectId}
+      />
+    );
   }
 
   return (
     <div className="h-screen mt-8 flex gap-8">
-      <ProjectsList projects={projects} addProjectStatusTrue={addProjectStatusTrue} setSelectedProject={setSelectedProject} viewProjectStatus={viewProjectStatus}/>
-
-      {addingProject ? <CreateProject/> : <NoProjectsPage addProjectStatusTrue={addProjectStatusTrue}/>}
-
-    {selectedProject &&   <Project selectedProject={selectedProject} deleteProject={deleteProject}/>}
-      </div>
+      <ProjectsList
+        projects={projectState.projects}
+        onStartAppProject={handleStartAppProject}
+        handleChooseProject={handleChooseProject}
+      />
+      {content}
+    </div>
   );
 }
 
--- src/components/CreateProject.jsx
+++ src/components/CreateProject.jsx
@@ -1,16 +1,83 @@
-export function CreateProject() {
-    return(
-        <form className=" mt-4 text-right">
-            <button className="px-6 py-2 text-left rounded-md my-1 hover:text-stone-200 hover:bg-stone-800">Cancel</button>
-            <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button>
-           <div className="text-left">
-           <label htmlFor ='name' className="text-sm font-bold uppercase text-stone-500">Title</label>
-           <input id="name" className=" mb-8 w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" />
-           <label htmlFor ='description' className="text-sm font-bold uppercase text-stone-500">Description</label>
-            <input id="description" className="mb-8  w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" />
-            <label htmlFor ='name' className="text-sm font-bold uppercase text-stone-500">Due Date</label>
-            <input id='date' type='date' className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" />
-           </div>
-        </form>
-    )
+import { useRef } from "react";
+
+export function CreateProject({projects, createNewProject, cancelProjectCreation}) {
+  const title = useRef('');
+  const description = useRef('');
+  const dueDate = useRef('');
+
+  let newProject = {
+    title: "",
+    description: "",
+    dueDate: "",
+    id: Math.random(),
+    tasks: [],
+  };
+
+  function handleSave(){
+    newProject.title = title.current.value;
+    newProject.description = description.current.value;
+    newProject.dueDate = dueDate.current.value;
+
+    console.log(newProject)
+    console.log(projects)
+    createNewProject(newProject)
+  } 
+
+  function handleCancel() {
+    cancelProjectCreation()
+  }
+
+  return (
+    <div className=" mt-4 text-right" >
+      <button className="px-6 py-2 text-left rounded-md my-1 hover:text-stone-200 hover:bg-stone-800" 
+      onClick={handleCancel} >
+        Cancel
+      </button>
+      <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+    type="submit"
+    onClick={handleSave}
+      >
+        Save
+      </button>
+      <div className="text-left">
+        <label
+          htmlFor="name"
+          className="text-sm font-bold uppercase text-stone-500"
+        >
+          Title
+        </label>
+        <input
+          ref={title}
+          id="name"
+          required
+          className=" mb-8 w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+        />
+        <label
+          htmlFor="description"
+          className="text-sm font-bold uppercase text-stone-500"
+        >
+          Description
+        </label>
+        <input
+          ref={description}
+          id="description"
+          required
+          className="mb-8  w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+        />
+        <label
+          htmlFor="name"
+          className="text-sm font-bold uppercase text-stone-500"
+        >
+          Due Date
+        </label>
+        <input
+          ref={dueDate}
+          id="date"
+          type="date"
+          required
+          className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+        />
+      </div>
+    </div>
+  );
 }
